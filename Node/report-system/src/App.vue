7b0d8a1ec051cc3d0ef7d855d5b8d5f5334d6fa4
Red <13191648829@163.com>
2021-02-21 08:25:48 +0800

    20210221

---
+++ Node/report-system/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex" router mode="horizontal">
+          <el-menu-item index="/">Home</el-menu-item>
+          <el-menu-item index="/about">About</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      activeIndex: "/",
+    };
+  },
+  created: {},
+  components: {},
+};
+</script>
+<style>
+@import url("./assets/base.css");
+</style>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.nav-link {
+  text-decoration: none;
+}
+</style>
---
+++ Node/report-system/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    This is Home page
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
